8411bf33a0c5e443e4ced9730cfda809f05f568e
DazzaH100 Henry <henrydarran@yahoo.com>
2024-04-04 16:05:52 +0000

    transferring code from other project

---
+++ assets/js/particles.js
@@ -0,0 +1,8 @@
+particlesJS('particles-js', {
+    particles: {
+        number: { value: 80, density: { enable: true, value_area: 800 } },
+        color: { value: ['#000000', '#6A82FB', '#A4EBF3', '#ffe'] },
+        /* ... additional options ... */
+    },
+    /* ... other particles.js settings ... */
+});
---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+/* If you are initializing particles.js in script.js */
+document.addEventListener('DOMContentLoaded', () => {
+    particlesJS('particles-js', {
+        /* particles.js config here */
+        /* ... */
+    });
+});
